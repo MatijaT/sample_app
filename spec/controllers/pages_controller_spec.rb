24f9239bf369c7a607a565a5b7b34541c300a2c0
unknown <Matija@.(none)>
2011-01-23 22:09:33 +0100

    done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+	it "treba da ima pravi naslov"do
+	get 'home'
+	response.should have_selector("title",
+							:content=>"|Home")
+	end
   end
 
   describe "GET 'contact'" do
@@ -14,6 +20,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+	it "treba da ima pravi naslov"do
+	get 'contact'
+	response.should have_selector("title",
+							:content=>"|Contact")
+	end
   end
+
+  describe "GET 'about'" do
+	it "should be successful" do
+	get 'about'
+	response.should be_success
+	end
+	it "treba da ima pravi naslov"do
+	get 'about'
+	response.should have_selector("title",
+							:content=>"|About")
+	end
+
+end
 
 end
